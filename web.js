f9e788ee5ad3064afb0eea56e4cff50c37a3e965
Ubuntu <ubuntu@ip-172-31-37-102.us-west-2.compute.internal>
2013-07-14 19:52:12 +0000

    Using readFileSync now instead

--- web.js
+++ web.js
@@ -3,12 +3,11 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
- 	fs.readFile("index.html", function(err, data) {
- 		response.send(data.ToString());
- 	}); 
+	var html = fs.readFileSync("index.html", "utf8"); 
+	response.send(html);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+	console.log("Listening on " + port);
 });
